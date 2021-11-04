fd34bc773154cf011fceaf445f663770de5cd2bb
Zhile <zhuzhile08@gmail.com>
2021-11-04 20:47:59 +0100

    fixes

--- src/ChoccoEngine/util/math.h
+++ src/ChoccoEngine/util/math.h
@@ -7,7 +7,7 @@ namespace ChoccoEngine {
 	struct Vector2 {
 	public:
 		double x, y;
-		Vector2() = default; 
+		Vector2(); 
 		Vector2(double x, double y);
 
 		Vector2 operator*(double a); Vector2 operator*(Vector2 a);

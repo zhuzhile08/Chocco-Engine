1221da3991893736f99b18bc5efe1da3ddf83357
Zhile <zhuzhile08@gmail.com>
2021-11-21 12:21:55 +0100

    fix bugs

--- cmake/sdl2_image-config.cmake
+++ cmake/sdl2_image-config.cmake
@@ -2,9 +2,9 @@ set(SDL2_IMAGE_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/include/SDL/")
 
 # Support both 32 and 64 bit builds
 if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
-  set(SDL2_IMAGE_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/x64/SDL2_image.lib")
+  set(SDL2_IMAGE_LIBRARIES "${CMAKE_SOURCE_DIR}/data/lib/x64/SDL2_image.lib")
 else ()
-  set(SDL2_IMAGE_LIBRARIES "${CMAKE_SOURCE_DIR}/lib/x86/SDL2_image.lib")
+  set(SDL2_IMAGE_LIBRARIES "${CMAKE_SOURCE_DIR}/data/lib/x86/SDL2_image.lib")
 endif ()
 
 string(STRIP "${SDL2_IMAGE_LIBRARIES}" SDL2_IMAGE_LIBRARIES)

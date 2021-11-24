79ccd2d0fd88a32d656c8f49b5c85546a8249d4a
Zhile <zhuzhile08@gmail.com>
2021-11-24 21:04:56 +0100

    implemented object groups

--- src/ChoccoEngine/obj/group.cpp
+++ src/ChoccoEngine/obj/group.cpp
@@ -1,18 +1,31 @@
 #include <ChoccoEngine/spriteGroup.h>
 
 namespace chocco {
-	/**
-	 * Sprite Groups
-	 * Sprite Groups are just a Vector storing GameObjs
-	 * there is a funtion in Game.h/cpp that will loop though the vector in this class and draw it
-	 * this can also be used to group sprites, for example for collision checks of so on
-	**/
-
-	void SpriteGroup::add(std::shared_ptr<Sprite> obj) {
-		group.insert(std::pair<std::string, std::shared_ptr<Sprite>>(obj->get_name(), obj));
+	// functions for the SpriteGroup
+	void SpriteGroup::add(Sprite obj) {
+		group.insert(std::pair<std::string, Sprite>(obj.getName(), obj));
 	}
 
-	void SpriteGroup::remove(std::string name, int check) {
+	void SpriteGroup::remove(std::string name) {
+		group.erase(name);
+	}
+
+	void SpriteGroup::update() {
+		for (std::pair<std::string, Sprite> pair : group) {
+			pair.second.update();
+		}
+	}
+
+	std::map <std::string, Sprite> SpriteGroup::getGroup() {
+		return group;
+	}
+
+	// functions for the ObjGroup
+	void ObjGroup::add(Object obj) {
+		group.insert(std::pair<std::string, Object>(obj.getName(), obj));
+	}
+
+	void ObjGroup::remove(std::string name, int check) {
 		if (check){
 			group.erase(name);
 		} else group.erase(name);

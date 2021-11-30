53ae65999c6a8eea21ea328ce84b066ddeee11fc
Zhile <zhuzhile08@gmail.com>
2021-11-30 16:48:22 +0100

    rename musicPlayer file

---
+++ include/ChoccoEngine/audio.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <SDL_mixer.h>
+
+namespace chocco {
+    class Sfx {
+    protected:
+        std::string path;
+    public:
+
+    };
+
+    class DstSfx {
+    protected:
+        std::string path;
+    public:
+    };
+}
